0e5e62663fed3c2d18700f52499c602b559f4544
Kunal Asude <143272678+KunalAsude@users.noreply.github.com>
2026-04-01 12:43:06 +0530

    conditio one

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(''),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/ayush'),
     analyzeModule],
   controllers: [AppController],
   providers: [AppService],
